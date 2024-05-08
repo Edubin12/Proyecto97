37675c5fbf44e9e62d2e68a0987c8943ed688b17
Edubin12 <156468763+Edubin12@users.noreply.github.com>
2024-05-08 13:42:03 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+// Crea la función addUser()
+function addUser() {
+  // Obtén el valor del usuario con id player1_name_input y player2_name_input
+  id: player1_name_input 
+  id:player2_name_input
+
+  // Almacena los valores en el almacenamiento local
+document.getElementById("local").innerHTML;
+  //Asigna "game_page.html" a window.location
+  window.location(game_page.htaml);
+}
+
